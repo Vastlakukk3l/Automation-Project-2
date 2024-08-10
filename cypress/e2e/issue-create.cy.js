e7d2ec12f0ac0420947965aecd046c04d9fe0629
Vastlakukk3l <martinivanov3@gmail.com>
2024-08-10 21:34:22 +0300

    Testing stuff

--- cypress/e2e/issue-create.cy.js
+++ cypress/e2e/issue-create.cy.js
@@ -1,3 +1,4 @@
+import { faker } from '@faker-js/faker';
 describe('Issue create', () => {
   beforeEach(() => {
     cy.visit('/');
@@ -13,32 +14,33 @@ describe('Issue create', () => {
     // System finds modal for creating issue and does next steps inside of it
     cy.get('[data-testid="modal:issue-create"]').within(() => {
       // Type value to description input field
-      cy.get('.ql-editor').type('TEST_DESCRIPTION');
-      cy.get('.ql-editor').should('have.text', 'TEST_DESCRIPTION');
+      cy.get('.ql-editor').type('My bug description');
+      cy.get('.ql-editor').should('have.text', 'My bug description');
 
       // Type value to title input field
       // Order of filling in the fields is first description, then title on purpose
       // Otherwise filling title first sometimes doesn't work due to web page implementation
-      cy.get('input[name="title"]').type('TEST_TITLE');
-      cy.get('input[name="title"]').should('have.value', 'TEST_TITLE');
+      cy.get('input[name="title"]').type('Bug');
+      cy.get('input[name="title"]').should('have.value', 'Bug');
 
       // Open issue type dropdown and choose Story
       cy.get('[data-testid="select:type"]').click();
-      cy.get('[data-testid="select-option:Story"]').wait(1000).trigger('mouseover').trigger('click');
-      cy.get('[data-testid="icon:story"]').should('be.visible');
+      cy.get('[data-testid="select-option:Bug"]').wait(200).trigger('mouseover').trigger('click');
 
       // Select Baby Yoda from reporter dropdown
       cy.get('[data-testid="select:reporterId"]').click();
-      cy.get('[data-testid="select-option:Baby Yoda"]').click();
+      cy.get('[data-testid="select-option:Pickle Rick"]').click();
 
       // Select Baby Yoda from assignee dropdown
       cy.get('[data-testid="form-field:userIds"]').click();
-      cy.get('[data-testid="select-option:Pickle Rick"]').click();
+      cy.get('[data-testid="select-option:Lord Gaben"]').click();
+
+      cy.get('[data-testid="select:priority"]').click();
+      cy.get('[data-testid="select-option:Highest"]').wait(1000).trigger("mouseover").trigger("click");
 
       // Click on button "Create issue"
       cy.get('button[type="submit"]').click();
     });
-
     // Assert that modal window is closed and successful message is visible
     cy.get('[data-testid="modal:issue-create"]').should('not.exist');
     cy.contains('Issue has been successfully created.').should('be.visible');
@@ -74,7 +76,42 @@ describe('Issue create', () => {
         cy.get('[data-testid="avatar:Pickle Rick"]').should('be.visible');
         cy.get('[data-testid="icon:story"]').should('be.visible');
       });
-  });
+    }); 
+
+    it('Should create an issue and validate it successfully whit random data', () => {
+      const randomTitle = faker.lorem.word();
+      const randomDescription = faker.lorem.words(5);
+
+      cy.get(".ql-editor").type(randomDescription);
+      cy.get(".ql-editor").should("have.text", randomDescription);
+
+     
+      cy.get('[data-testid="select:type"]').click();
+      cy.get('[data-testid="select-option:Bug"]').wait(200).trigger('mouseover').trigger('click');
+      cy.get('[data-testid="select:type"]').click();
+      cy.get('[data-testid="select-option:Task"]').wait(200).trigger('mouseover').trigger('click');
+
+      
+      cy.get('[data-testid="select:reporterId"]').click();
+      cy.get('[data-testid="select-option:Baby Yoda"]').click();
+
+
+      cy.get('[data-testid="select:priority"]').click();
+      cy.get('[data-testid="select-option:Low"]').wait(1000).trigger("mouseover").trigger("click");
+
+      
+      cy.get('button[type="submit"]').click();
+    });
+   
+    
+    
+    
+    
+    
+    
+    
+    
+  
 
   it('Should validate title is required field if missing', () => {
     // System finds modal for creating issue and does next steps inside of it
